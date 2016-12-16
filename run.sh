7fd151a4a6d8a134be1e0fb226b90a54dfeb2d20
Sergio Garcia <sergio@garciparedes.me>
2016-12-16 11:45:12 +0100

    Created octave file and execution script

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo '--------------------------------------------------------------------------------'
+echo 'Title: Hopfield Network: Cube Aproximation'
+echo 'Subject: Data Mining'
+echo 'Author: Sergio García Prado (garciparedes.me)'
+echo '--------------------------------------------------------------------------------'
+echo 'Octave:'
+echo
+octave src/hopfield.m
+echo '--------------------------------------------------------------------------------'
