631d7f41c1e175c24903c21732582034ba01d199
Sergio Garcia <sergio@garciparedes.me>
2016-12-16 17:10:16 +0100

    Worked on hop field network implementation

--- src/hopfield.m
+++ src/hopfield.m
@@ -8,19 +8,48 @@ clear all
 
 
 x = [
-    -1, -1,  1;
-     1, -1, -1;
-     1,  1,  1;
-    -1,  1,  1;
-    -1, -1, -1;
-     1,  1, -1;
+     -1, -1,  1;
+    #   1, -1, -1;
+    #  1,  1,  1;
+    # -1,  1,  1;
+    # -1, -1, -1;
+    #  1,  1, -1;
 ]
 
-a1 = [ 1, -1,  1]
-a2 = [-1,  1, -1]
+x_p = [
+     1, -1,  1;
+    -1,  1, -1;
+];
 
-w = [
-    0, -2, 2;
-    -2, 0, -2;
-    2, -2, 0;
-]
+w = zeros(size(x,2),size(x,2));
+
+for i = 1:size(w,1);
+    for j = 1:size(w,2);
+        if( i == j);
+            w(i,j) = 0;
+        else;
+            sum1 = 0;
+            for p = 1: size(x_p,1);
+                sum1 =+  x_p(p,i) * x_p(p,j);
+            endfor;
+            w(i,j) = sum1;
+        endif;
+    endfor;
+endfor;
+w
+
+
+s = x;
+for i = 1:size(w,1);
+    do;
+        s_copy = s;
+
+        temp = sum(w(i,:) .* s_copy);
+        if (temp > 0);
+            s(i) = 1;
+        elseif(temp < 0);
+            s(i) = -1;
+        endif;
+    until(s_copy == s);
+endfor;
+s
