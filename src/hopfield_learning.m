919d5edcb28a97c874fa8199d96d69a929354773
Sergio Garcia <sergio@garciparedes.me>
2016-12-23 10:40:16 +0100

    Indent changes

--- src/hopfield_learning.m
+++ src/hopfield_learning.m
@@ -1,10 +1,10 @@
 function w = hopfield_learning(x_p)
   w = zeros(size(x_p,2),size(x_p,2));
   for i = 1:size(w,1);
-      for j = 1:size(w,2);
-          if(i ~= j);
-              w(i,j) = sum(x_p(:,i) .* x_p(:,j));
-          endif;
-      endfor;
+    for j = 1:size(w,2);
+      if(i ~= j);
+        w(i,j) = sum(x_p(:,i) .* x_p(:,j));
+      endif;
+    endfor;
   endfor;
 end
--- src/hopfield_working.m
+++ src/hopfield_working.m
@@ -1,15 +1,15 @@
 function s = hopfield_working(w,s)
-    for index = 1:size(s,1);
-        do;
-            for i = 1:size(s,2);
-                s_old = s(index,:);
-                temp = sum(w(i,:) .* s_old);
-                if (temp > 0);
-                    s(index,i) = 1;
-                elseif(temp < 0);
-                    s(index,i) = -1;
-                endif;
-            endfor;
-        until(s_old == s(index,:));
-    endfor;
+  for index = 1:size(s,1);
+    do;
+      for i = 1:size(s,2);
+        s_old = s(index,:);
+        temp = sum(w(i,:) .* s_old);
+        if (temp > 0);
+          s(index,i) = 1;
+        elseif(temp < 0);
+          s(index,i) = -1;
+        endif;
+      endfor;
+    until(s_old == s(index,:));
+  endfor;
 end;
