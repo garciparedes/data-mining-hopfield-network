500f0fb30a69d258d916a458a4fb4766c2b6572f
Sergio Garcia <sergio@garciparedes.me>
2016-12-24 12:26:27 +0100

    Send practice

--- src/main.m
+++ src/main.m
@@ -1,12 +1,10 @@
 function main()
   clear all;
 
-  X = [-1, -1, 1;   1, -1, -1;   1, 1, 1;   -1, 1, 1;   -1, -1, -1;   1, 1, -1];
-  P = [1, -1, 1;   -1, 1, -1];
+  X = [-1, -1, 1;   1, -1, -1;   1, 1, 1;   -1, 1, 1;   -1, -1, -1;   1, 1, -1]
+  P = [1, -1, 1;   -1, 1, -1]
 
-  W = hopfield_learning(P);
-  S = hopfield_working(W, X);
-
-  disp('X ='), disp(X), disp('P ='), disp(P);
-  disp('W ='), disp(W), disp('S ='), disp(S);
+  W = hopfield_learning(P)
+  S = hopfield_working(W, X)
 end;
+main();
